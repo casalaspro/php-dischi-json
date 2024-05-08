421be9be6e09e6a7b01448430939cfe4ad7db550
casalaspro <casalaspro.alessandro@gmail.com>
2024-05-08 15:26:52 +0200

    ADD - stack links and files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./style/app.css">
+  <title>Dischi Php Json</title>
+</head>
+<body>
+  <div id="app">
+    <header>
+      <img src="./assets/spotify-logo.png" alt="" class="logo">
+    </header>
+
+    <main>
+
+    </main>
+
+    
+  </div>
+
+
+
+
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+  
+</body>
+</html>
