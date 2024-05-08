b1400ee745427519327e46ac1f86dc9a5239e5cb
casalaspro <casalaspro.alessandro@gmail.com>
2024-05-08 16:27:44 +0200

    REFACTORY and ADD - app.js

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./style/app.css">
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   <title>Dischi Php Json</title>
 </head>
 <body>
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
 
 <script>
 
@@ -59,8 +61,8 @@
 
 
 
-<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
+
   
 </body>
 </html>
